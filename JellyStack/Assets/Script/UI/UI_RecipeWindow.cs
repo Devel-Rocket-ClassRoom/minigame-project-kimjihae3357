b7ceda65af5c187802572680b23109bff8a5aaab
김지해 <bmj1644035@gmail.com>
2026-05-28 23:14:07 +0900

    레시피ui 더미추가
    - 토글형식으로 열고 닫고 방식으로 구현

---
+++ JellyStack/Assets/Script/UI/UI_RecipeWindow.cs
@@ -0,0 +1,25 @@
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class UI_Recipe : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private TMP_Text buttonText;
+    [SerializeField] private GameObject panel;
+
+    private void Start()
+    {
+        panel.gameObject.SetActive(false);
+        button.onClick.AddListener(TogglePanel);
+    }
+
+    private void TogglePanel()
+    {
+        bool isOpen = !panel.activeSelf;
+        panel.SetActive(isOpen);
+        buttonText.text = isOpen ? "<-" : "->";
+    }
+
+
+}
