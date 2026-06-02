ac154448d054746162d172ce42f53b4ae27c6aff
김지해 <bmj1644035@gmail.com>
2026-06-02 11:53:22 +0900

    이펙트 추가
    - 주민 죽는 이펙트 추가
    - 카드팩 오픈 이펙트 추가

--- JellyStack/Assets/Script/CardPack/OpenPack.cs
+++ JellyStack/Assets/Script/CardPack/OpenPack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class PackCard : Card
 {
     [SerializeField] private CardPackData packData;   // StarterPack / RandomCardPack 등 어떤 팩 타입이든 받을 수 있음
-    [Tooltip("카드 한 장이 나올 때 스폰될 이펙트 프리팹. null이면 이펙트 없음.")]
+    [Header("카드 오픈 이펙트")]
     [SerializeField] private GameObject spawnEffectPrefab;
 
     private List<CardData> remainingCards;
